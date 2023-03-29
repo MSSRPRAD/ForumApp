845f76477b16e6a974aa985c7a1d6e2fab0f1201
Pradyumna Malladi <f20210367@hyderabad.bits-pilani.ac.in>
2023-03-29 18:50:32 +0530

    Added functionality to 1) Send Friend Requests 2) Accept Friend Requests 3) View Friends.

--- ForumApp/models/user.py
+++ ForumApp/models/user.py
@@ -9,16 +9,47 @@ from ForumApp.models.comment import Comment
 from ForumApp.models.profile import Profile
 from ForumApp.models.notification import Notification
 from ForumApp.models.board import Board
+from sqlalchemy.ext.associationproxy import association_proxy
 
 app = Flask(__name__)
 
+friendship = db.Table(
+    'friendships',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), index=True),
+    db.Column('friend_id', db.Integer, db.ForeignKey('user.id')),
+    db.UniqueConstraint('user_id', 'friend_id', name='unique_friendships'))
+
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable = False)
-    password = db.Column(db.String(100), nullable = False)
+    username = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
-    profile = db.relationship('Profile', backref='user', uselist = False)
+    profile = db.relationship('Profile', backref='user', uselist=False)
     notifications = db.relationship('Notification', backref='user')
-    posts = db.relationship('Post', backref = 'user')
+    posts = db.relationship('Post', backref='user')
     boards = db.relationship('Board', backref='user')
-    comments = db.relationship('Comment', backref = 'user')
+    comments = db.relationship('Comment', backref='user')
+    requests = association_proxy('received_rels', 'requesting_user')
+    requested = association_proxy('requested_rels', 'receiving_user')
+
+    requested_rels = db.relationship(
+        'FriendRequest',
+        foreign_keys='FriendRequest.requesting_user_id',
+        backref='requesting_user'
+    )
+    received_rels = db.relationship(
+        'FriendRequest',
+        foreign_keys='FriendRequest.receiving_user_id',
+        backref='receiving_user'
+    )
+
+    friends = db.relationship('User',
+                              secondary=friendship,
+                              primaryjoin=id == friendship.c.user_id,
+                              secondaryjoin=id == friendship.c.friend_id)
+
+class FriendRequest(db.Model):
+    requesting_user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    receiving_user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    status = db.Column(db.Integer)
--- ForumApp/prof/routes.py
+++ ForumApp/prof/routes.py
@@ -1,5 +1,8 @@
+import dbus.server
+
+from ForumApp.models.role import Role
 from ForumApp.prof import bp
-from flask import render_template
+from flask import render_template, redirect
 from flask_login import login_required, login_user, LoginManager, current_user, logout_user
 from flask_bcrypt import Bcrypt
 from flask import current_app as app
@@ -7,6 +10,8 @@ bcrypt = Bcrypt(app)
 from ForumApp.models.user import User
 from ForumApp.models.profile import Profile
 from ForumApp.models.notification import Notification
+from ForumApp.models.user import FriendRequest
+from ForumApp import db
 
 @app.login_manager.user_loader
 def load_user(user_id):
@@ -21,11 +26,66 @@ def index():
     elif current_user.role_id == 2:
         role_name = "USER"
     profile = Profile.query.filter_by(user_id =current_user.id).first()
-    return render_template('src/profile/profile.html', user = current_user, role_name = role_name, profile=profile)
+    return render_template('src/profile/profile.html', user = current_user, role_name = role_name, profile=profile, cond=False)
+
+@bp.route('/users/<int:id>')
+@login_required
+def list_users(id):
+    user = User.query.filter_by(id=id).first()
+    role_name = user.role.role
+    profile = user.profile
+    return render_template('src/profile/profile.html', user = user, role_name = role_name, profile=profile, cond=True)
+
 
 @bp.route('/profile/notifications')
 @login_required
 def notif():
     notifications = Notification.query.filter_by(user_id = current_user.id).order_by(Notification.date_created.desc()).all()
-
     return render_template('src/profile/notification.html', user=current_user, notifications = notifications)
+
+@bp.route('/users')
+@login_required
+def view_user():
+    users = User.query.all()
+    return render_template('src/profile/users.html', users = users)
+
+@bp.route('/users/request/<int:id>/')
+@login_required
+def request(id):
+    existing = FriendRequest.query.filter_by(requesting_user_id=current_user.id, receiving_user_id=id).first()
+    if existing:
+        url = '/users/'+str(id)
+        return redirect(url)
+    else:
+        friendRequest = FriendRequest()
+        friendRequest.requesting_user_id = current_user.id
+        friendRequest.receiving_user_id = id
+        friendRequest.status = 0
+        db.session.add(friendRequest)
+        db.session.commit()
+        notif = Notification()
+        notif.user_id = id
+        notif.message = current_user.username + ' has sent you a friend request!'
+        db.session.add(notif)
+        db.session.commit()
+    return redirect('/users')
+
+@bp.route('/profile/friends')
+@login_required
+def show_friends():
+    friends = current_user.friends
+    requests = FriendRequest.query.filter_by(receiving_user_id=current_user.id).all()
+    return render_template('src/profile/friends.html', friends = friends, requests=requests, users = User.query.all(), current_user=current_user)
+
+
+@bp.route('/users/request/<int:id>/accept')
+@login_required
+def accept_request(id):
+    request = FriendRequest.query.filter_by(requesting_user_id=id, receiving_user_id = current_user.id).first()
+    db.session.delete(request)
+    db.session.commit()
+    user = User.query.filter_by(id=id).first()
+    user.friends.append(current_user)
+    current_user.friends.append(user)
+    db.session.commit()
+    return redirect('/profile/friends')
